24ad1ad1be0dbfb8ed7d502ca19c79954051b489
Winnie3315 <winniiemae777@gmail.com>
2024-04-21 21:42:27 +0500

    v1.2

--- mac.js
+++ mac.js
@@ -6,56 +6,41 @@ let price = document.querySelector('.price')
 
 let memoryBtns = document.querySelectorAll('.memory-item')
 
-let macs = {
-    'whiteMac': {
-        'activeBtn': 'active-btn',
-        'colorName': 'White',
-        'imgUrl': './img/mac-white.png'
-    },
-    'greyMac': {
-        'activeBtn': 'active-btn',
-        'colorName': 'Space Gray',
-        'imgUrl': './img/mac-gray.png'
-    }
-}
-
-for (let mac in macs) {
-    let btn = document.querySelector(`#${mac}`);
-    btn.onclick = () => {
-        whiteMac.classList.remove('active-btn');
-        greyMac.classList.remove('active-btn');
-        
-        btn.classList.add(macs[mac].activeBtn);
-        colorName.innerText = macs[mac].colorName;
-        macImg.style.backgroundImage = `url('${macs[mac].imgUrl}')`;
-    }
-}
+let whitePrice = '$1,999';
+let greyPrice = '$2,599';
 
 
-let priceList = {
-    '200': '$2,199',
-    '600': '$2,599',
-    '1,200': '$3,199',
-    'default': '$1,999'
+whiteMac.onclick = () => {
+    greyMac.classList.remove('active-btn')
+    whiteMac.classList.add('active-btn')
+    colorName.innerText = 'White'
+    macImg.style.backgroundImage = `url('./img/mac-white.png')`
+    price.innerText = whitePrice
+};
+
+greyMac.onclick = () => {
+    whiteMac.classList.remove('active-btn')
+    greyMac.classList.add('active-btn')
+    colorName.innerText = 'Space Gray'
+    macImg.style.backgroundImage = `url('./img/mac-gray.png')`
+    price.innerText = greyPrice
 }
+
 
 memoryBtns.forEach(btn => {
     btn.onclick = () => {
-        memoryBtns.forEach(btn => {
-            btn.classList.remove('active-mem')
-        })
+        memoryBtns.forEach(btn => btn.classList.remove('active-mem'))
         btn.classList.add('active-mem')
-
-        let priceText = priceList['default'];
-
-
-        for(let itm in priceList){
-            if (btn.innerText.includes(itm)) {
-                priceText = priceList[itm]; 
-                break;
-            }
+        
+        
+        if (btn.innerText.includes('512 GB')) {
+            price.innerText = greyMac.classList.contains('active-btn') ? '$2,599' : '$1,999'
+        } else if (btn.innerText.includes('1 TB')) {
+            price.innerText = greyMac.classList.contains('active-btn') ? '$2,799' : '$2,199'
+        } else if (btn.innerText.includes('2 TB')) {
+            price.innerText = greyMac.classList.contains('active-btn') ? '$3,199' : '$2,599' 
+        } else if (btn.innerText.includes('4 TB')) {
+            price.innerText = greyMac.classList.contains('active-btn') ? '$3,799' : '$3,199' 
         }
-        price.innerText = priceText;
     }
 })
-  
